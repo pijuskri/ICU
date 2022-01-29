aea3611cbb8ac594eab0b1d37ef57e79aeb684a6
Dragoș <balandragos5555@gmail.com>
2022-01-30 00:52:09 +0100

    Throw mechanic + dynamite explod after 3s

--- Assets/Scripts/PickableItem.cs
+++ Assets/Scripts/PickableItem.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class PickableItem : MonoBehaviour
 {
     // Reference to the rigidbody
-    private Rigidbody rb;
-    public Rigidbody Rb => rb;
+    private Rigidbody _rb;
+    public Rigidbody Rb => _rb;
+
+    public bool fuse = false;
+    public float timeRemaining;
+    
     /// <summary>
     /// Method called on initialization.
     /// </summary>
     private void Awake()
     {
         // Get reference to the rigidbody
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        if (fuse)
+            if (timeRemaining > 0)
+                timeRemaining -= Time.deltaTime;
+            else
+            {
+                Instantiate(GameLogic.instance.explodeAnime, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
     }
 }
--- Assets/Scripts/SimpleGrabSystem.cs
+++ Assets/Scripts/SimpleGrabSystem.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 
 
-public class SimpleGrabSystem : MonoBehaviour
-{
+public class SimpleGrabSystem : MonoBehaviour {
     // Reference to the character camera.
     [SerializeField]
     private GameObject characterCamera;
@@ -10,37 +9,36 @@ public class SimpleGrabSystem : MonoBehaviour
     [SerializeField]
     private Transform slot;
     // Reference to the currently held item.
-    private PickableItem pickedItem;
+    private PickableItem _pickedItem;
+    private Vector3 OGscale;
+
     /// <summary>
     /// Method called very frame.
     /// </summary>
-    private void Update()
-    {
+    private void Update() {
         // Execute logic only on button pressed
         if (Input.GetButtonDown("Fire1"))
         {
             // Check if player picked some item already
-            if (pickedItem)
+            if (_pickedItem)
             {
                 // If yes, drop picked item
-                DropItem(pickedItem);
+                DropItem(_pickedItem);
             }
             else
             {
                 // If no, try to pick item in front of the player
                 // Create ray from center of the screen
-                // var ray = characterCamera.ViewportPointToRay(Vector3.one);
                 Vector3 origin = characterCamera.transform.position;
                 Vector3 direction = characterCamera.transform.forward;
                 RaycastHit hit;
                 // Shot ray to find object to pick
                 if (Physics.Raycast(origin, direction, out hit, 3f))
                 {
-                    Debug.DrawRay(origin, direction * hit.distance, Color.red, 50f);
                     // Check if object is pickable
                     var pickable = hit.transform.GetComponent<PickableItem>();
                     // If object has PickableItem class
-                    if (pickable)
+                    if (pickable && !pickable.fuse)
                     {
                         // Pick it
                         PickItem(pickable);
@@ -48,38 +46,67 @@ public class SimpleGrabSystem : MonoBehaviour
                 }
             }
         }
+
+        if (Input.GetButtonDown("Fire2") && _pickedItem)
+        {
+            ThrowItem(_pickedItem);
+        }
     }
+    
     /// <summary>
     /// Method for picking up item.
     /// </summary>
     /// <param name="item">Item.</param>
-    private void PickItem(PickableItem item)
-    {
+    private void PickItem(PickableItem item) {
         // Assign reference
-        pickedItem = item;
+        _pickedItem = item;
         // Disable rigidbody and reset velocities
         item.Rb.isKinematic = true;
         item.Rb.velocity = Vector3.zero;
         item.Rb.angularVelocity = Vector3.zero;
+        OGscale = item.transform.localScale;
         // Set Slot as a parent
-        item.transform.SetParent(slot);
+        item.transform.SetParent(slot, true);
         // Reset position and rotation
         item.transform.position = slot.transform.position;
         item.transform.eulerAngles = slot.transform.eulerAngles;
+        Debug.Log(item.transform.localScale);
     }
+    
     /// <summary>
     /// Method for dropping item.
     /// </summary>
     /// <param name="item">Item.</param>
-    private void DropItem(PickableItem item)
-    {
+    private void DropItem(PickableItem item) {
         // Remove reference
-        pickedItem = null;
+        _pickedItem = null;
         // Remove parent
-        item.transform.SetParent(null);
+        item.transform.SetParent(null, true);
+        // Reset to original scale
+        item.transform.localScale = OGscale;
         // Enable rigidbody
         item.Rb.isKinematic = false;
         // Add force to throw item a little bit
         item.Rb.AddForce(item.transform.forward * 2, ForceMode.VelocityChange);
+    }
+
+    /// <summary>
+    /// Method for throwing item.
+    /// </summary>
+    /// <param name="item">Item.</param>
+    private void ThrowItem(PickableItem item) {
+        // Remove reference
+        _pickedItem = null;
+        // Remove parent
+        item.transform.SetParent(null, true);
+        // Reset to original scale
+        item.transform.localScale = OGscale;
+        // Enable rigidbody
+        item.Rb.isKinematic = false;
+        // Add force to throw item a little bit
+        item.Rb.AddForce(item.transform.forward * 10, ForceMode.VelocityChange);
+
+        item.fuse = true;
+        item.timeRemaining = 5;
     }
 }
