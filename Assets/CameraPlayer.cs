a166b0bbacc8c097976ff9ea82617025f5de2960
Pijus Krisiukenas <pijuskri@gmail.com>
2022-01-29 02:13:58 +0100

    ortho

--- Assets/CameraPlayer.cs
+++ Assets/CameraPlayer.cs
@@ -7,9 +7,10 @@ using UnityEngine.Rendering;
 public class CameraPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    Camera camera;
     void Start()
     {
+        camera = GetComponent<Camera>();
         RenderPipelineManager.beginCameraRendering += BeginCameraRendering;
         RenderPipelineManager.endCameraRendering += EndCameraRendering;
     }
@@ -21,20 +22,27 @@ public class CameraPlayer : MonoBehaviour
         int xmove = Convert.ToInt32(Input.GetKey(KeyCode.A)) - Convert.ToInt32(Input.GetKey(KeyCode.D));
         int ymove = Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S));
         int zoom = Convert.ToInt32(Input.GetKey(KeyCode.Q)) - Convert.ToInt32(Input.GetKey(KeyCode.E));
-        transform.position += ((Vector3.left * xmove) + (Vector3.forward * ymove)) * Time.deltaTime * speed;
-        transform.position += Vector3.up * zoom * Time.deltaTime * ZoomSpeed();
+        transform.position += ((Vector3.left * xmove) + (Vector3.forward * ymove)) * Time.deltaTime * ZoomSpeed() * 0.75f;
+        //transform.position += Vector3.up * zoom * Time.deltaTime * ZoomSpeed() * 0.75f;
+        camera.orthographicSize = Mathf.Clamp( camera.orthographicSize + (zoom* ZoomSpeed() * 0.005f), 3, 50);
     }
+    /*
     float ZoomSpeed() {
-        return Mathf.Clamp(transform.position.y, 3, 20) * zoomSpeed;
+        return Mathf.Clamp(transform.position.y, 3, 25);
+    }
+    */
+    float ZoomSpeed()
+    {
+        return Mathf.Clamp(camera.orthographicSize, 5, 30);
     }
     void BeginCameraRendering(ScriptableRenderContext src, Camera cam) {
-        if (GetComponent<Camera>() == cam) {
+        if (camera == cam) {
             RenderSettings.fog = false;
         }
     }
-    void EndCameraRendering(ScriptableRenderContext context, Camera camera)
+    void EndCameraRendering(ScriptableRenderContext context, Camera cam)
     {
-        if (GetComponent<Camera>() == camera)
+        if (camera == cam)
         {
             RenderSettings.fog = true;
         }
