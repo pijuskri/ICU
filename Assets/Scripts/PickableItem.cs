cc06bc7ec584b5440071993d2e78a94940c9a3c3
Dragoș <balandragos5555@gmail.com>
2022-01-29 20:05:56 +0100

    Dynamite pickup and drop
    and crosshair, pogers

--- Assets/Scripts/PickableItem.cs
+++ Assets/Scripts/PickableItem.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody))]
 public class PickableItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Reference to the rigidbody
+    private Rigidbody rb;
+    public Rigidbody Rb => rb;
+    /// <summary>
+    /// Method called on initialization.
+    /// </summary>
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Get reference to the rigidbody
+        rb = GetComponent<Rigidbody>();
     }
 }
---
+++ Assets/Scripts/SimpleGrabSystem.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+
+public class SimpleGrabSystem : MonoBehaviour
+{
+    // Reference to the character camera.
+    [SerializeField]
+    private GameObject characterCamera;
+    // Reference to the slot for holding picked item.
+    [SerializeField]
+    private Transform slot;
+    // Reference to the currently held item.
+    private PickableItem pickedItem;
+    /// <summary>
+    /// Method called very frame.
+    /// </summary>
+    private void Update()
+    {
+        // Execute logic only on button pressed
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Check if player picked some item already
+            if (pickedItem)
+            {
+                // If yes, drop picked item
+                DropItem(pickedItem);
+            }
+            else
+            {
+                // If no, try to pick item in front of the player
+                // Create ray from center of the screen
+                // var ray = characterCamera.ViewportPointToRay(Vector3.one);
+                Vector3 origin = characterCamera.transform.position;
+                Vector3 direction = characterCamera.transform.forward;
+                RaycastHit hit;
+                // Shot ray to find object to pick
+                if (Physics.Raycast(origin, direction, out hit, 3f))
+                {
+                    Debug.DrawRay(origin, direction * hit.distance, Color.red, 50f);
+                    // Check if object is pickable
+                    var pickable = hit.transform.GetComponent<PickableItem>();
+                    // If object has PickableItem class
+                    if (pickable)
+                    {
+                        // Pick it
+                        PickItem(pickable);
+                    }
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// Method for picking up item.
+    /// </summary>
+    /// <param name="item">Item.</param>
+    private void PickItem(PickableItem item)
+    {
+        // Assign reference
+        pickedItem = item;
+        // Disable rigidbody and reset velocities
+        item.Rb.isKinematic = true;
+        item.Rb.velocity = Vector3.zero;
+        item.Rb.angularVelocity = Vector3.zero;
+        // Set Slot as a parent
+        item.transform.SetParent(slot);
+        // Reset position and rotation
+        item.transform.position = slot.transform.position;
+        item.transform.eulerAngles = slot.transform.eulerAngles;
+    }
+    /// <summary>
+    /// Method for dropping item.
+    /// </summary>
+    /// <param name="item">Item.</param>
+    private void DropItem(PickableItem item)
+    {
+        // Remove reference
+        pickedItem = null;
+        // Remove parent
+        item.transform.SetParent(null);
+        // Enable rigidbody
+        item.Rb.isKinematic = false;
+        // Add force to throw item a little bit
+        item.Rb.AddForce(item.transform.forward * 2, ForceMode.VelocityChange);
+    }
+}
